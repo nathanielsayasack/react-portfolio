41a61b0a4fcb2445b60592ca1e89ceb94bec432a
nathanielsayasack <ntsyrs@uw.edu>
2025-04-18 14:55:25 -0700

    added to hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,8 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Nathaniel</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+        I am Senior at UW majoring in Informatics; aspiring product manager passionate about FinTech, innovation, and creating impactful digital solutions.
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
