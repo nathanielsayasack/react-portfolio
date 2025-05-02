bb94554f9fb59b3cade37fcd6f36ad46dd75376a
nathanielsayasack <ntsyrs@uw.edu>
2025-05-02 13:53:45 -0700

    updated about portion of site

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,36 +10,34 @@ export const About = () => {
       <div className={styles.content}>
         <img
           src={getImageUrl("about/aboutImage.JPG")}
-          alt="Me sitting with a laptop"
+          alt="Me posing in front of a walkway"
           className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <img src={getImageUrl("about/bulbIcon.png")} alt="bulb icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>Product Strategy</h3>
               <p>
-              I am Senior at UW majoring in Informatics; aspiring product manager passionate about FinTech, innovation, and creating impactful digital solutions.
+              I define clear product goals and roadmaps by identifying user needs, aligning with business objectives, and collaborating with stakeholders to deliver impactful solutions.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <img src={getImageUrl("about/barChart.png")} alt="bar chart icon" />
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
+              <h3>User Research & Data-Driven Decisions</h3>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+                I conduct user research and analyze data to guide product direction—translating insights into actionable features and continuously iterating based on feedback.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <img src={getImageUrl("about/handshake.jpg")} alt="Handshake Icon" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              <h3>Cross-Functional Collaboration</h3>
               <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+              I work closely with design, engineering, and business teams to ensure product alignment, drive execution, and foster a shared vision from ideation to launch.
               </p>
             </div>
           </li>
